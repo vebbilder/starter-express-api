f02c7948a293e420edf4699da1298e2d678322f7
vebbilder <66235658+vebbilder@users.noreply.github.com>
2023-08-11 20:43:44 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,23 @@
+{
+    "token": "MTA5NzU3MDg2NTg2MTg4NjEwMw.GVqyzk.Dx1kS2led0w4gMFLkgM9cnqwaurqxphhrWp5og",
+    "prefix": "?",
+    "cfg": {
+        "intents": [
+            "GUILDS",
+            "GUILD_BANS",
+            "GUILD_EMOJIS_AND_STICKERS",
+            "GUILD_INTEGRATIONS",
+            "GUILD_WEBHOOKS",
+            "GUILD_INVITES",
+            "GUILD_VOICE_STATES",
+            "GUILD_MESSAGES",
+            "GUILD_MESSAGE_REACTIONS",
+            "GUILD_MESSAGE_TYPING",
+            "DIRECT_MESSAGES",
+            "DIRECT_MESSAGE_REACTIONS",
+            "GUILD_VOICE_STATES",
+            "GUILD_MESSAGES",
+            "DIRECT_MESSAGE_TYPING"
+        ]
+    }
+}
